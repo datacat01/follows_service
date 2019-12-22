277e1d0b6074c399ec685d4c95106f443ea9cebd
Olha Pashnieva <olha.pashnieva@gmail.com>
2019-12-22 19:29:42 +0200

    minor fixes

--- lib/follows_service.ex
+++ lib/follows_service.ex
@@ -1,51 +1,55 @@
 defmodule FollowsService do
   import Ecto.Query, only: [from: 2]
 
-  def add_flwr_flwd(flwr_id, flwd_id) do
-    flwr_flwd = %Follows.FlwrFlwdPair{}
-
-    flwr_flwd = %{flwr_flwd | uuid: Ecto.UUID.bingenerate()}
-    flwr_flwd = %{flwr_flwd | follower_id: flwr_id}
-    flwr_flwd = %{flwr_flwd | followed_id: flwd_id}
-
+  def select_query(flwr_id, flwd_id) do
     query = from(
       f in "follower_followed",
       where: f.follower_id == ^flwr_id and f.followed_id == ^flwd_id,
       select: f.uuid,
       limit: 1
     )
+  end
+
+  def ini_struct(flwr_id, flwd_id, uuid) do
+    flwr_flwd = %Follows.FlwrFlwdPair{}
+
+    flwr_flwd = %{flwr_flwd | uuid: uuid}
+    flwr_flwd = %{flwr_flwd | follower_id: flwr_id}
+    flwr_flwd = %{flwr_flwd | followed_id: flwd_id}
+  end
+
+
+  def follow(flwr_id, flwd_id) do
+    uuid = Ecto.UUID.bingenerate()
+    flwr_flwd = ini_struct(flwr_id, flwd_id, uuid)
+    query = select_query(flwr_id, flwd_id)
 
     Follows.Repo.transaction(fn repo ->
       if List.first(repo.all(query)) == nil do 
         repo.insert(flwr_flwd)
       end
     end)
+
+    flwr_flwd
   end
 
 
-  def del_flwr_flwd(flwr_id, flwd_id) do
+  def unfollow(flwr_id, flwd_id) do
 
-    query = from(
-      f in "follower_followed",
-      where: f.follower_id == ^flwr_id and f.followed_id == ^flwd_id,
-      select: f.uuid,
-      limit: 1
-    )
+    query = select_query(flwr_id, flwd_id)
 
     Follows.Repo.transaction(fn repo ->
-      if List.first(repo.all(query)) != nil do
-
-        flwr_flwd = %Follows.FlwrFlwdPair{}
-        flwr_flwd = %{flwr_flwd | uuid: List.first(repo.all(query))}
-        flwr_flwd = %{flwr_flwd | follower_id: flwr_id}
-        flwr_flwd = %{flwr_flwd | followed_id: flwd_id}
-
-        repo.delete(flwr_flwd)
+      cond do
+        List.first(repo.all(query)) != nil ->
+          uuid = List.first(repo.all(query))
+          flwr_flwd = ini_struct(flwr_id, flwd_id, uuid)
+          repo.delete(flwr_flwd)
+          uuid
+        true ->
+          nil
       end
     end)
   end
 
-  def hello do
-    :world
-  end
+  
 end
--- priv/repo/migrations/20191222124922_create_flwr_flwd.exs
+++ priv/repo/migrations/20191222124922_create_flwr_flwd.exs
@@ -6,6 +6,7 @@ defmodule Follows.Repo.Migrations.CreateFlwrFlwd do
       add :uuid, :uuid, primary_key: true
       add :follower_id, :string
       add :followed_id, :string
+      add :followed_id, :string
     end
   end
 end
