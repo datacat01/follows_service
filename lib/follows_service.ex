29273f0b57d65183bfdd165bb1eec3180db5b932
Olha Pashnieva <olha.pashnieva@gmail.com>
2019-12-22 18:03:57 +0200

    unfollow

--- lib/follows_service.ex
+++ lib/follows_service.ex
@@ -14,9 +14,35 @@ defmodule FollowsService do
       select: f.uuid,
       limit: 1
     )
-    if List.first(Follows.Repo.all(query)) == nil do
-      Follows.Repo.insert(flwr_flwd)
-    end
+
+    Follows.Repo.transaction(fn repo ->
+      if List.first(repo.all(query)) == nil do 
+        repo.insert(flwr_flwd)
+      end
+    end)
+  end
+
+
+  def del_flwr_flwd(flwr_id, flwd_id) do
+
+    query = from(
+      f in "follower_followed",
+      where: f.follower_id == ^flwr_id and f.followed_id == ^flwd_id,
+      select: f.uuid,
+      limit: 1
+    )
+
+    Follows.Repo.transaction(fn repo ->
+      if List.first(repo.all(query)) != nil do
+
+        flwr_flwd = %Follows.FlwrFlwdPair{}
+        flwr_flwd = %{flwr_flwd | uuid: List.first(repo.all(query))}
+        flwr_flwd = %{flwr_flwd | follower_id: flwr_id}
+        flwr_flwd = %{flwr_flwd | followed_id: flwd_id}
+
+        repo.delete(flwr_flwd)
+      end
+    end)
   end
 
   def hello do
--- lib/follows_service/flwr_flwd_pair.ex
+++ lib/follows_service/flwr_flwd_pair.ex
@@ -4,7 +4,7 @@ defmodule Follows.FlwrFlwdPair do
     @primary_key false
   
     schema "follower_followed" do
-      field :uuid, :string
+      field :uuid, :string, primary_key: true
       field :follower_id, :string
       field :followed_id, :string
     end
